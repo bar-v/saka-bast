f84cc243517328af9883eedcdac35cdbeef511ca
bar-v <akbarsy8@gmail.com>
2024-10-18 16:18:34 +0700

    penambahan file blade baru

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -5,11 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Beranda</title>
 </head>
 
 <body>
+    <div>
+        <h1> ANRI </h1>
+    </div>
+    <div>
 
+    </div>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('beranda');
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -17,4 +18,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
