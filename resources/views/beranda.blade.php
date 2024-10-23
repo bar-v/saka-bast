a2d81ac50234cd7bf68ac4b573ff64f8ba15cf5a
bar-v <akbarsy8@gmail.com>
2024-10-23 08:16:34 +0700

    publish

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -5,15 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
     <title>Beranda</title>
 </head>
 
 <body>
-    <div>
-        <h1> ANRI </h1>
+    <div class="header">
+        <h2> <img src=""> ANRI </h2>
+        <div>
+            <h3> SEARCH <img src=""></h3>
+        </div>
     </div>
-    <div>
-
+    <div class="isi">
+        <h2>bagaimana</h2>
     </div>
 </body>
 
