f8b28711648694f07dfcefcbf58ec582bbcdafc3
bar-v <akbarsy8@gmail.com>
2024-10-23 09:35:11 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,15 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/beranda', function () {
+    return view('beranda');
+});
+
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/beranda', function () {
     return view('beranda');
 });
 
