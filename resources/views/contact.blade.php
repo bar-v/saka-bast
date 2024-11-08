f115312b5c53b56dd960833da63a239279e8a196
Hapid12 <buttertiger022@gmail.com>
2024-11-08 09:42:56 +0700

    hapid imut

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,19 @@
 <x-app-layout>
     <x-slot name="header">
-
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Contact Us') }}
         </h2>
     </x-slot> <br>
 
-    <div class="kaka">
-        <H2 href="/dashboard"> Kembali </H2>
-    </div>
-</x-app-layout>
+    
+
+        <!-- Kotak putih di tengah -->
+        <div class="flex justify-center items-center min-h-screen">
+            <!-- warna ama ukuran ny Kotak putih -->
+            <div class="bg-white w-3/4 min-h-auto rounded-lg shadow-lg p-6">
+                <!-- teks dalam kotak -->
+                <p>Informasi Administrator</p>
+            </div>
+        </div>
+
+    </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,14 +1,16 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700 ">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:py-8" >
+    <div class="mx-auto px-4 sm:px-6 lg:py-8" >
         <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                    </a>
-                </div>
+        <div class="flex justify-start">
+    <!-- Logo -->
+    <div class="shrink-0 flex items-center">
+        <a href="{{ route('dashboard') }}">
+            <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+        </a>
+    </div>
+    
+
 
                 <!-- Navigation Links -->
                 <div class="bg-gray-900 rounded-full inline-flex items-center">
