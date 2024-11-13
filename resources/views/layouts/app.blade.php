480db15c2a51300b0606e597d1addc4cbd5fb2db
MRaditFebriansyah <muhammadradit2021@gmail.com>
2024-11-13 18:00:54 +0700

    ubah title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'SAKA BAST') }}</title>
-           
+        <link rel="icon" type="image/svg+xml" href="{{ asset('image/image.svg') }}">           
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
