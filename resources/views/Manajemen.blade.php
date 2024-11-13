62a3415fe01e65a7247aa83cbcf8511dcdc15514
MRaditFebriansyah <muhammadradit2021@gmail.com>
2024-11-13 16:14:45 +0700

    update title

--- resources/views/Manajemen.blade.php
+++ resources/views/Manajemen.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    
     <!-- Tambahkan link ke Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Tambahkan jQuery dan Bootstrap JS -->
