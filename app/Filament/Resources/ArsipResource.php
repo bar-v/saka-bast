ab206a5363fd7e33472f7542cd414ad4b90b4a94
KioxDy <kioaep14@gmail.com>
2024-11-13 08:23:30 +0700

    filament

---
+++ app/Filament/Resources/ArsipResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ArsipResource\Pages;
+use App\Filament\Resources\ArsipResource\RelationManagers;
+use App\Models\Arsip;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ArsipResource extends Resource
+{
+    protected static ?string $model = Arsip::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListArsips::route('/'),
+            'create' => Pages\CreateArsip::route('/create'),
+            'edit' => Pages\EditArsip::route('/{record}/edit'),
+        ];
+    }
+}
