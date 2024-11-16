da7baeb9f28882696326f5924d9165fe7777dee5
KioxDy <kioaep14@gmail.com>
2024-11-16 17:55:26 +0700

    DB nullable change

--- database/migrations/2024_11_14_083543_create_arsips_table.php
+++ database/migrations/2024_11_14_083543_create_arsips_table.php
@@ -13,25 +13,25 @@ return new class extends Migration
     {
         Schema::create('arsip', function (Blueprint $table) {
             $table->id();
-            $table->integer('nomor_arsip'); // Nomor Arsip
-            $table->string('kode_pelaksana', 100); // Kode Pelaksana
-            $table->string('kode_klasifikasi');
-            $table->string('kode_satker');
-            $table->string('nama_unit_pengolah');
-            $table->text('uraian_informasi_arsip');
-            $table->year('tahun_awal');
+            $table->integer('nomor_arsip')->nullable(); // Nomor Arsip
+            $table->string('kode_pelaksana', 100)->nullable(); // Kode Pelaksana
+            $table->string('kode_klasifikasi')->nullable();
+            $table->string('kode_satker')->nullable();
+            $table->string('nama_unit_pengolah')->nullable();
+            $table->text('uraian_informasi_arsip')->nullable();
+            $table->year('tahun_awal')->nullable();
             $table->year('tahun_akhir')->nullable();
-            $table->string('tingkat_perkembangan');
-            $table->string('media_simpan');
-            $table->string('jumlah_berkas');
-            $table->string('kondisi_fisik');
-            $table->string('ukuran');
+            $table->string('tingkat_perkembangan')->nullable();
+            $table->string('media_simpan')->nullable();
+            $table->string('jumlah_berkas')->nullable();
+            $table->string('kondisi_fisik')->nullable();
+            $table->string('ukuran')->nullable();
             $table->string('keterangan')->nullable();
-            $table->string('ruang');
-            $table->integer('lemari');
-            $table->integer('boks');
-            $table->string('jenis_arsip');
-            $table->string('alih_media');
+            $table->string('ruang')->nullable();
+            $table->integer('lemari')->nullable();
+            $table->integer('boks')->nullable();
+            $table->string('jenis_arsip')->nullable();
+            $table->string('alih_media')->nullable();
             $table->timestamps();
         });
     }
